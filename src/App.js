1d924e4cfedf19c8c35899c454655fe7b67080f3
Erebus <felixm.manchester@gmail.com>
2018-10-05 15:36:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavbarLBS from './components/NavbarLBS';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavbarLBS />
+      </div>
+    );
+  }
+}
+
+export default App;
